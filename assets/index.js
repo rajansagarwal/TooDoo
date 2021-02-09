cb9e0b91b789d9d8013555c0c5d7458faaefc067
Rajan Agarwal <64426829+itsrajan@users.noreply.github.com>
2021-02-09 17:10:49 -0500

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -60,124 +60,62 @@ function strike(index) {
     display();
 }
 
-var elements = [];
-window.onload = function() {
-    if (JSON.parse(localStorage.getItem("elements")) != null) elements = JSON.parse(localStorage.getItem("elements"));
-    console.log(elements);
-    display();
-};
+const el = document.querySelector(".clock");
+const bell = document.querySelector("audio");
 
-function addElement() {
-    if (document.querySelector(".addTxt").value.trim() != "") {
-        elements.push(document.querySelector(".addTxt").value.trim());
-        if (localStorage.getItem("elements") == null) {
-            localStorage.setItem("elements", JSON.stringify(elements));
-        } else {
-            localStorage.setItem("elements", JSON.stringify(elements));
-        }
-        display();
-    }
-}
+const mindiv = document.querySelector(".mins");
+const secdiv = document.querySelector(".secs");
 
-function display() {
-    document.querySelector(".list").innerHTML = "";
-    for (var i = 0; i < elements.length; i++) document.querySelector(".list").innerHTML += "<center><div class='element'>" + elements[i] + "<img class='tick' src = 'https://i2.wp.com/entman.com/wp-content/uploads/2016/09/checkmark.png?fit=256%2C256&ssl=1' onclick='strike(" + i + ")'><img class='dustbin' src = 'https://img.icons8.com/bubbles/2x/trash.png' onclick='del(" + i + ")'></div></center><br>";
-}
+const startBtn = document.querySelector(".start");
+localStorage.setItem("btn", "focus");
 
-function del(index) {
-    elements.splice(index, 1);
-    if (localStorage.getItem("elements") == null) {
-        localStorage.setItem("elements", JSON.stringify(elements));
-    } else {
-        localStorage.setItem("elements", JSON.stringify(elements));
-    }
-    display();
-}
+let initial, totalsecs, perc, paused, mins, seconds;
 
-function strike(index) {
-    if (elements[index].includes("<strike>")) {
-        elements[index] = elements[index].replace("<strike>", "");
-    } else {
-        elements[index] = "<strike>" + elements[index] + "</strike>";
-    }
-    if (localStorage.getItem("elements") == null) {
-        localStorage.setItem("elements", JSON.stringify(elements));
-    } else {
-        localStorage.setItem("elements", JSON.stringify(elements));
-    }
-    display();
-}
+startBtn.addEventListener("click", () => {
+  let btn = localStorage.getItem("btn");
 
-function strike(index) {
-    if (elements[index].includes("<strike>")) {
-        elements[index] = elements[index].replace("<strike>", "");
-    } else {
-        elements[index] = "<strike>" + elements[index] + "</strike>";
-    }
-    if (localStorage.getItem("elements") == null) {
-        localStorage.setItem("elements", JSON.stringify(elements));var elements = [];
-window.onload = function() {
-    if (JSON.parse(localStorage.getItem("elements")) != null) elements = JSON.parse(localStorage.getItem("elements"));
-    console.log(elements);
-    display();
-};
+  if (btn === "focus") {
+    mins = +localStorage.getItem("focusTime") || 1;
+  } else {
+    mins = +localStorage.getItem("breakTime") || 1;
+  }
 
-function addElement() {
-    if (document.querySelector(".addTxt").value.trim() != "") {
-        elements.push(document.querySelector(".addTxt").value.trim());
-        if (localStorage.getItem("elements") == null) {
-            localStorage.setItem("elements", JSON.stringify(elements));
-        } else {
-            localStorage.setItem("elements", JSON.stringify(elements));
-        }
-        display();
-    }
-}
+  seconds = mins * 60;
+  totalsecs = mins * 60;
+  setTimeout(decremenT(), 60);
+  paused = false;
+});
 
-function display() {
-    document.querySelector(".list").innerHTML = "";
-    for (var i = 0; i < elements.length; i++) document.querySelector(".list").innerHTML += "<center><div class='element'>" + elements[i] + "<img class='tick' src = 'https://i2.wp.com/entman.com/wp-content/uploads/2016/09/checkmark.png?fit=256%2C256&ssl=1' onclick='strike(" + i + ")'><img class='dustbin' src = 'https://img.icons8.com/bubbles/2x/trash.png' onclick='del(" + i + ")'></div></center><br>";
-}
+function decremenT() {
+  mindiv.textContent = Math.floor(seconds / 60);
+  secdiv.textContent = seconds % 60 > 9 ? seconds % 60 : `0${seconds % 60}`;
+  if (circle.classList.contains("danger")) {
+    circle.classList.remove("danger");
+  }
 
-function del(index) {
-    elements.splice(index, 1);
-    if (localStorage.getItem("elements") == null) {
-        localStorage.setItem("elements", JSON.stringify(elements));
-    } else {
-        localStorage.setItem("elements", JSON.stringify(elements));
+  if (seconds > 0) {
+    perc = Math.ceil(((totalsecs - seconds) / totalsecs) * 100);
+    setProgress(perc);
+    seconds--;
+    initial = window.setTimeout("decremenT()", 1000);
+    if (seconds < 10) {
+      circle.classList.add("danger");
     }
-    display();
-}
+  } else {
+    mins = 0;
+    seconds = 0;
+    bell.play();
+    let btn = localStorage.getItem("btn");
 
-function strike(index) {
-    if (elements[index].includes("<strike>")) {
-        elements[index] = elements[index].replace("<strike>", "");
+    if (btn === "focus") {
+      startBtn.textContent = "Start Break";
+      startBtn.classList.add("break");
+      localStorage.setItem("btn", "break");
     } else {
-        elements[index] = "<strike>" + elements[index] + "</strike>";
+      startBtn.classList.remove("break");
+      startBtn.textContent = "start focus";
+      localStorage.setItem("btn", "focus");
     }
-    if (localStorage.getItem("elements") == null) {
-        localStorage.setItem("elements", JSON.stringify(elements));
-    } else {
-        localStorage.setItem("elements", JSON.stringify(elements));
-    }
-    display();
-}
-
-function strike(index) {
-    if (elements[index].includes("<strike>")) {
-        elements[index] = elements[index].replace("<strike>", "");
-    } else {
-        elements[index] = "<strike>" + elements[index] + "</strike>";
-    }
-    if (localStorage.getItem("elements") == null) {
-        localStorage.setItem("elements", JSON.stringify(elements));
-    } else {
-        localStorage.setItem("elements", JSON.stringify(elements));
-    }
-    display();
-}
-    } else {
-        localStorage.setItem("elements", JSON.stringify(elements));
-    }
-    display();
+    startBtn.style.transform = "scale(1)";
+  }
 }
